486fbbf7280725fb2f53f40b27ae54673fe39df4
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-12-25 11:52:44 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -4,19 +4,39 @@ import Header from "./compononts/Header";
 import Product from "./compononts/Product";
 import Cart from "./compononts/Cart";
 import SimpleCart from "./compononts/SimpleCart";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import UserForm from "./compononts/cart/UserForm";
 
 function App() {
   return (
     <div className="App">
-       <Header />
-      <Catogries />
-      <Cart />
-      <SimpleCart />
-      < Product />
-      <Footer />
+      {/* <Header /> */}
 
-     
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
+        </nav>{" "}
+        <Switch>
+          <Route path="/cart">
+            <UserForm />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Catogries />
+            <Cart />
+            <SimpleCart />
+            <Product />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
